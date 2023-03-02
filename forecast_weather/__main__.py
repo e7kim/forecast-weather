37da552902e51dd5fb884e0958f05ea714ea5553
Eugene Kim <e7kim@DESKTOP-4Q7RFN0>
2023-03-02 01:21:40 -0500

    HW4

--- forecast_weather/__main__.py
+++ forecast_weather/__main__.py
@@ -1,4 +1,4 @@
-from .weather import print_hello
+from .weather import show_current
 
 if __name__ == "__main__":
-    print_hello()
+    show_current("10027", "1")
--- forecast_weather/weather.py
+++ forecast_weather/weather.py
@@ -1,42 +1,38 @@
 import requests
-import datetime as dt
 
-def hello() -> str:
-    return "Hello, world!"
-
-def print_hello() -> None:
-    print(hello())
 
 def get_api_key() -> str:
     return open('api_key', 'r').read()
 
-def build_url(lat: str, lon: str, part: str, key: str) -> str:
-    return f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={key}"
 
-def get_weather(lat: str, lon: str, part: str):
-    url = build_url(lat, lon, part, get_api_key())
+def build_url(key: str, location: str, days: str) -> str:
+    return f"http://api.weatherapi.com/v1/forecast.json?key={key}&q={location}&days={days}&aqi=yes&alerts=no"
+
+
+def get_weather(url: str):
     return requests.get(url).json()
 
-def kelvin_to_fahrenheit(temp: int) -> int:
-    return 9/5 * (temp - 273.15) + 32
 
-def show_current(lat: str, lon: str, part: str):
-    resp = get_weather(lat, lon, part)
-    temp = kelvin_to_fahrenheit(resp['current']['temp'])
-    feels_like = kelvin_to_fahrenheit(resp['current']['feels_like'])
-    pressure = resp['current']['feels_like']
-    humidity = resp['current']['humidity']
-    dew_point = resp['current']['dew_point']
-    uvi = resp['current']['uvi']
-    clouds = resp['current']['clouds']
-    wind_speed = resp['current']['wind_speed']
+def show_current(location: str, days: str):
+    url = build_url(get_api_key(), location, days)
+    data = get_weather(url)
+    name = data['location']['name']
+    temp_c = data['current']['temp_c']
+    temp_f = data['current']['temp_f']
+    condition = data['current']['condition']['text']
+    wind_mph = data['current']['wind_mph']
+    pressure = data['current']['pressure_mb']
+    precip = data['current']['precip_in']
+    humidity = data['current']['humidity']
+    cloud = data['current']['cloud']
+    uv = data['current']['uv']
 
-    print(f"Current weather at ({lat}, {lon}) is: ")
-    print(f"Temperature: {temp}")
-    print(f"Temperature feels like: {feels_like}")
-    print(f"Pressure: {pressure} mbar")
+    print(f"Current weather at {name} is: ")
+    print(f"Temperature: {temp_f} F/ {temp_c} C")
+    print(f"Condition: {condition}")
+    print(f"Wind speed: {wind_mph} mph")
+    print(f"Pressure: {pressure} mb")
+    print(f"Precipitation: {precip} in")
     print(f"Humidity: {humidity}%")
-    print(f"Dew Point: {dew_point}")
-    print(f"UVI: {uvi}")
-    print(f"Cloud coverage: {clouds}%")
-    print(f"Wind speed: {wind_speed}")
+    print(f"Cloud coverage: {cloud}%")
+    print(f"UV: {uv}")
