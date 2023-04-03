181c5bf40d9782dd8d1f04010ebfbd51b3ef9960
Eugene Kim <e7kim@DESKTOP-4Q7RFN0>
2023-04-03 13:29:45 -0400

    Expanded forecast features

--- forecast_weather/tests/test_all.py
+++ forecast_weather/tests/test_all.py
@@ -75,9 +75,9 @@ def test_show_current_NYC(mock_print):
                     },
                 },
             )
-            url = build_url(get_api_key(), "10027", "2")
+            url = build_url(get_api_key(), "10027", "1")
             data = get_weather(url)
-            show_current("10027", "1")
+            show_current("10027")
             assert mock_print.mock_calls == [
                 call("Current weather at New York is: "),
                 call("Temperature: 44.1 F/ 6.7 C"),
@@ -89,3 +89,230 @@ def test_show_current_NYC(mock_print):
                 call("Cloud coverage: 100%"),
                 call("UV: 3.0"),
             ]
+
+def test_get_current_NYC():
+    with patch('builtins.open', mock_open(read_data="mock_key")) as mock_file:
+        with patch('requests.get') as r_mock:
+            r_mock.return_value = Mock(
+                status_code=200,
+                json=lambda: {
+                    "location": {"name": "New York"},
+                    "current": {
+                        "temp_c": 6.7,
+                        "temp_f": 44.1,
+                        "condition": {"text": "Overcast"},
+                        "wind_mph": 2.2,
+                        "pressure_mb": 1021.0,
+                        "precip_in": 30.14,
+                        "humidity": 47,
+                        "cloud": 100,
+                        "uv": 3.0,
+                    },
+                },
+            )
+            url = build_url(get_api_key(), "10027", "1")
+            data = get_weather(url)
+            assert get_current("10027") == {
+                "name": "New York",
+                "temp_c": 6.7,
+                "temp_f": 44.1,
+                "condition": "Overcast",
+                "wind_mph": 2.2,
+                "pressure_mb": 1021.0,
+                "precip_in": 30.14,
+                "humidity": 47,
+                "cloud": 100,
+                "uv": 3.0,
+            }
+
+@patch('builtins.print')
+def test_show_forecast_NYC(mock_print):
+    with patch('builtins.open', mock_open(read_data="mock_key")) as mock_file:
+        with patch('requests.get') as r_mock:
+            r_mock.return_value = Mock(
+                status_code=200,
+                json=lambda: {
+                    "location": {"name": "New York"},
+                    "forecast": {
+                        "forecastday": [
+                            {
+                                "date": "2023-04-03",
+                                "date_epoch": 1680480000,
+                                "day": {
+                                    "maxtemp_c": 17.0,
+                                    "maxtemp_f": 62.6,
+                                    "mintemp_c": 2.7,
+                                    "mintemp_f": 36.9,
+                                    "avgtemp_c": 7.8,
+                                    "avgtemp_f": 46.0,
+                                    "maxwind_mph": 15.4,
+                                    "maxwind_kph": 24.8,
+                                    "totalprecip_mm": 0.0,
+                                    "totalprecip_in": 0.0,
+                                    "totalsnow_cm": 0.0,
+                                    "avgvis_km": 10.0,
+                                    "avgvis_miles": 6.0,
+                                    "avghumidity": 63.0,
+                                    "daily_will_it_rain": 0,
+                                    "daily_chance_of_rain": 0,
+                                    "daily_will_it_snow": 0,
+                                    "daily_chance_of_snow": 0,
+                                    "condition": {
+                                        "text": "Partly cloudy",
+                                        "icon": "//cdn.weatherapi.com/weather/64x64/day/116.png",
+                                        "code": 1003
+                                    },
+                                    "uv": 3.0
+                                },
+                            },
+                            {
+                                "date": "2023-04-04",
+                                "date_epoch": 1680566400,
+                                "day": {
+                                    "maxtemp_c": 19.1,
+                                    "maxtemp_f": 66.4,
+                                    "mintemp_c": 6.1,
+                                    "mintemp_f": 43.0,
+                                    "avgtemp_c": 13.1,
+                                    "avgtemp_f": 55.5,
+                                    "maxwind_mph": 8.7,
+                                    "maxwind_kph": 14.0,
+                                    "totalprecip_mm": 0.0,
+                                    "totalprecip_in": 0.0,
+                                    "totalsnow_cm": 0.0,
+                                    "avgvis_km": 10.0,
+                                    "avgvis_miles": 6.0,
+                                    "avghumidity": 78.0,
+                                    "daily_will_it_rain": 0,
+                                    "daily_chance_of_rain": 0,
+                                    "daily_will_it_snow": 0,
+                                    "daily_chance_of_snow": 0,
+                                    "condition": {
+                                        "text": "Cloudy",
+                                        "icon": "//cdn.weatherapi.com/weather/64x64/day/119.png",
+                                        "code": 1006
+                                    },
+                                    "uv": 3.0
+                                },
+                            }
+                        ]
+                    }
+                },
+            )
+            url = build_url(get_api_key(), "10027", "2")
+            data = get_weather(url)
+            show_forecast("10027", "2")
+            assert mock_print.mock_calls == [
+                call("2023-04-03 weather forecast of New York is: "),
+                call("Average temperature: 46.0 F/ 7.8 C"),
+                call("Condition: Partly cloudy"),
+                call("Max wind speed: 15.4 mph"),
+                call("Total precipitation: 0.0 in"),
+                call("Average humidity: 63.0%"),
+                call("UV: 3.0"),
+                call("2023-04-04 weather forecast of New York is: "),
+                call("Average temperature: 55.5 F/ 13.1 C"),
+                call("Condition: Cloudy"),
+                call("Max wind speed: 8.7 mph"),
+                call("Total precipitation: 0.0 in"),
+                call("Average humidity: 78.0%"),
+                call("UV: 3.0"),
+            ]
+
+
+def test_get_forecast_NYC():
+    with patch('builtins.open', mock_open(read_data="mock_key")) as mock_file:
+        with patch('requests.get') as r_mock:
+            r_mock.return_value = Mock(
+                status_code=200,
+                json=lambda: {
+                    "location": {"name": "New York"},
+                    "forecast": {
+                        "forecastday": [
+                            {
+                                "date": "2023-04-03",
+                                "date_epoch": 1680480000,
+                                "day": {
+                                    "maxtemp_c": 17.0,
+                                    "maxtemp_f": 62.6,
+                                    "mintemp_c": 2.7,
+                                    "mintemp_f": 36.9,
+                                    "avgtemp_c": 7.8,
+                                    "avgtemp_f": 46.0,
+                                    "maxwind_mph": 15.4,
+                                    "maxwind_kph": 24.8,
+                                    "totalprecip_mm": 0.0,
+                                    "totalprecip_in": 0.0,
+                                    "totalsnow_cm": 0.0,
+                                    "avgvis_km": 10.0,
+                                    "avgvis_miles": 6.0,
+                                    "avghumidity": 63.0,
+                                    "daily_will_it_rain": 0,
+                                    "daily_chance_of_rain": 0,
+                                    "daily_will_it_snow": 0,
+                                    "daily_chance_of_snow": 0,
+                                    "condition": {
+                                        "text": "Partly cloudy",
+                                        "icon": "//cdn.weatherapi.com/weather/64x64/day/116.png",
+                                        "code": 1003
+                                    },
+                                    "uv": 3.0
+                                },
+                            },
+                            {
+                                "date": "2023-04-04",
+                                "date_epoch": 1680566400,
+                                "day": {
+                                    "maxtemp_c": 19.1,
+                                    "maxtemp_f": 66.4,
+                                    "mintemp_c": 6.1,
+                                    "mintemp_f": 43.0,
+                                    "avgtemp_c": 13.1,
+                                    "avgtemp_f": 55.5,
+                                    "maxwind_mph": 8.7,
+                                    "maxwind_kph": 14.0,
+                                    "totalprecip_mm": 0.0,
+                                    "totalprecip_in": 0.0,
+                                    "totalsnow_cm": 0.0,
+                                    "avgvis_km": 10.0,
+                                    "avgvis_miles": 6.0,
+                                    "avghumidity": 78.0,
+                                    "daily_will_it_rain": 0,
+                                    "daily_chance_of_rain": 0,
+                                    "daily_will_it_snow": 0,
+                                    "daily_chance_of_snow": 0,
+                                    "condition": {
+                                        "text": "Cloudy",
+                                        "icon": "//cdn.weatherapi.com/weather/64x64/day/119.png",
+                                        "code": 1006
+                                    },
+                                    "uv": 3.0
+                                },
+                            }
+                        ]
+                    }
+                },
+            )
+            url = build_url(get_api_key(), "10027", "2")
+            data = get_weather(url)
+            assert get_forecast("10027", "2") == {
+                'name': 'New York',
+                '2023-04-03': {
+                    'avgtemp_f': 46.0,
+                    'avgtemp_c': 7.8,
+                    'condition': "Partly cloudy",
+                    'maxwind_mph': 15.4,
+                    'totalprecip_in': 0.0,
+                    'avghumidity': 63.0,
+                    'uv': 3.0
+                },
+                '2023-04-04': {
+                    'avgtemp_f': 55.5,
+                    'avgtemp_c': 13.1,
+                    'condition': "Cloudy",
+                    'maxwind_mph': 8.7,
+                    'totalprecip_in': 0.0,
+                    'avghumidity': 78.0,
+                    'uv': 3.0
+                }
+            }
--- forecast_weather/weather.py
+++ forecast_weather/weather.py
@@ -13,26 +13,54 @@ def get_weather(url: str):
     return requests.get(url).json()
 
 
-def show_current(location: str, days: str):
+def show_current(location: str):
+    url = build_url(get_api_key(), location, 1)
+    data = get_weather(url)
+    print(f"Current weather at {data['location']['name']} is: ")
+    print(f"Temperature: {data['current']['temp_f']} F/ {data['current']['temp_c']} C")
+    print(f"Condition: {data['current']['condition']['text']}")
+    print(f"Wind speed: {data['current']['wind_mph']} mph")
+    print(f"Pressure: {data['current']['pressure_mb']} mb")
+    print(f"Precipitation: {data['current']['precip_in']} in")
+    print(f"Humidity: {data['current']['humidity']}%")
+    print(f"Cloud coverage: {data['current']['cloud']}%")
+    print(f"UV: {data['current']['uv']}")
+
+
+def get_current(location: str):
+    url = build_url(get_api_key(), location, 1)
+    data = get_weather(url)
+    curr = {'name': data['location']['name'], 'condition': data['current']['condition']['text']}
+    for key in ['temp_c', 'temp_f', 'wind_mph', 'pressure_mb', 'precip_in', 'humidity', 'cloud', 'uv']:
+        curr[key] = data['current'][key]
+    return curr
+
+
+def show_forecast(location: str, days: str):
     url = build_url(get_api_key(), location, days)
     data = get_weather(url)
-    name = data['location']['name']
-    temp_c = data['current']['temp_c']
-    temp_f = data['current']['temp_f']
-    condition = data['current']['condition']['text']
-    wind_mph = data['current']['wind_mph']
-    pressure = data['current']['pressure_mb']
-    precip = data['current']['precip_in']
-    humidity = data['current']['humidity']
-    cloud = data['current']['cloud']
-    uv = data['current']['uv']
+    for i in range(len(data['forecast']['forecastday'])):
+        print(f"{data['forecast']['forecastday'][i]['date']} weather forecast of {data['location']['name']} is: ")
+        print(f"Average temperature: {data['forecast']['forecastday'][i]['day']['avgtemp_f']} F/ {data['forecast']['forecastday'][i]['day']['avgtemp_c']} C")
+        print(f"Condition: {data['forecast']['forecastday'][i]['day']['condition']['text']}")
+        print(f"Max wind speed: {data['forecast']['forecastday'][i]['day']['maxwind_mph']} mph")
+        print(f"Total precipitation: {data['forecast']['forecastday'][i]['day']['totalprecip_in']} in")
+        print(f"Average humidity: {data['forecast']['forecastday'][i]['day']['avghumidity']}%")
+        print(f"UV: {data['forecast']['forecastday'][i]['day']['uv']}")
 
-    print(f"Current weather at {name} is: ")
-    print(f"Temperature: {temp_f} F/ {temp_c} C")
-    print(f"Condition: {condition}")
-    print(f"Wind speed: {wind_mph} mph")
-    print(f"Pressure: {pressure} mb")
-    print(f"Precipitation: {precip} in")
-    print(f"Humidity: {humidity}%")
-    print(f"Cloud coverage: {cloud}%")
-    print(f"UV: {uv}")
+
+def get_forecast(location: str, days: str):
+    url = build_url(get_api_key(), location, days)
+    data = get_weather(url)
+    forecast = {'name': data['location']['name']}
+    for i in range(len(data['forecast']['forecastday'])):
+        day = {}
+        day['avgtemp_f'] = data['forecast']['forecastday'][i]['day']['avgtemp_f']
+        day['avgtemp_c'] = data['forecast']['forecastday'][i]['day']['avgtemp_c']
+        day['condition'] = data['forecast']['forecastday'][i]['day']['condition']['text']
+        day['maxwind_mph'] = data['forecast']['forecastday'][i]['day']['maxwind_mph']
+        day['totalprecip_in'] = data['forecast']['forecastday'][i]['day']['totalprecip_in']
+        day['avghumidity'] = data['forecast']['forecastday'][i]['day']['avghumidity']
+        day['uv'] = data['forecast']['forecastday'][i]['day']['uv']
+        forecast[data['forecast']['forecastday'][i]['date']] = day
+    return forecast
